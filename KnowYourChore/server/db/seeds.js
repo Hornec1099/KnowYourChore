fefa220a785e63728a645cb6d9f61ed92efa6c91
Cameron <Cameronhorne@Hotmail.co.uk>
2021-09-16 09:51:33 +0100

    added server and seeds

---
+++ KnowYourChore/server/db/seeds.js
@@ -0,0 +1,27 @@
+use tasks;
+
+db.dropDatabase();
+
+db.tasks.insertMany([
+    { "id": 1,
+    "taskName": "Deep clean oven",
+    "taskDescription": "Clean oven using products under sink",
+    "taskCompleteBy": "4.00 pm",
+    "taskLocation": "Kitchen",
+    "taskAssignedTo": "Cameron"
+    },
+    { "id": 2,
+    "taskName": "Vacuum",
+    "taskDescription": "Vacuum bedrooms",
+    "taskCompleteBy": "7.30 pm",
+    "taskLocation": "Bedrooms",
+    "taskAssignedTo": "Arek"
+    },
+    { "id": 3,
+    "taskName": "Clean bathroom",
+    "taskDescription": "Clean your bloody bathroom",
+    "taskCompleteBy": "2:00 pm",
+    "taskLocation": "En-Suite",
+    "taskAssignedTo": "Akwasi"
+    }
+]);
---
+++ KnowYourChore/server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+MongoClient.connect("mongodb://localhost:27017").then(() => {
+  const db = client.db("tasks");
+  const tasksCollection = db.collection("tasks");
+  const tasksRouter = createRouter(tasksCollection);
+  app.use("/api/tasks", tasksRouter);
+})
+.catch(console.error)
+
+
+
+app.listen(5000, function(){
+    console.log(`Listening on ${this.address().port}`)
+})
