70e9a07dde3108b21ed2343f54328416ffb27d68
Cameron <Cameronhorne@Hotmail.co.uk>
2021-09-16 09:01:52 +0100

    updated folders

--- KnowYourChore/app/App.js
+++ KnowYourChore/app/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
