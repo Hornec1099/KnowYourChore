8405df8f028d7210a0c882df6a7338e0050bcfee
Cameron <Cameronhorne@Hotmail.co.uk>
2021-09-16 12:07:58 +0100

    worked on front end slightly

--- KnowYourChore/app/App.js
+++ KnowYourChore/app/App.js
@@ -1,14 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import taskService from "./services/tasksService";
 import TextComponent from "./textComponent";
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  const getAllTasks = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/tasks/");
+      const data = await response.json();
+      setTasks(data);
+      console.log(data);
+    } catch {
+      (error) => {
+        console.error(error);
+      };
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    taskService.getTasks().then((tasks) => setTasks(tasks));
+    getAllTasks();
   }, []);
+
   console.log(tasks);
-  return <TextComponent tasks={tasks} />;
+
+  return (
+    <View>
+      { isLoading ? <ActivityIndicator/> : (
+      <TextComponent tasks={tasks} />
+      )};
+    </View>
+  );
 }
--- KnowYourChore/app/services/tasksService.js
+++ KnowYourChore/app/services/tasksService.js
@@ -1,10 +1,15 @@
-const baseUrl = 'http://localhost:5000/api/tasks/';
+const baseUrl = "http://localhost:5000/api/tasks/";
 
 const taskService = {
-    getTasks(){
-        return fetch(`${baseUrl}`)
-        .then(res => res.json())
-    }
-}
+  getTasks() {
+    return fetch(baseUrl)
+      .then((res) => res.json())
+      
+  },
+};
 
 export default taskService;
+
+
+
+
