81c1dd48c4b944c056f8cf085bd82f4558961134
Arek Rucki <45738613+kafejkarz@users.noreply.github.com>
2021-09-19 14:23:46 +0100

    drop list added

--- KnowYourChore/app/screens/SelectionScreen.js
+++ KnowYourChore/app/screens/SelectionScreen.js
@@ -1,28 +1,76 @@
-import React from "react";
-import { Dropdown } from "react-native-material-dropdown";
-import { Text , Button, View} from "react-native";
+import React, { useState } from "react";
+// import { Dropdown } from "react-native-material-dropdown";
+import { Text , Button, View, StyleSheet, Picker} from "react-native";
+// import { Ionicons} from '@expo/vector-icons';
+// import { Colors } from "react-native/Libraries/NewAppScreen";
 
 function SelectionScreen({navigation}){
     
+    const [selectedValue, setSelectedValue] = useState("java");
     
-    let data = [{
-        value: "TaskList1"
-    },
-    {
-        value: "TaskList2"
-    }]
+    // let data = [{
+    //     value: "TaskList1"
+    // },
+    // {
+    //     value: "TaskList2"
+    // }]
     
     return (
 
-        <View>
-            <Text> Choose Your Chores </Text>
+        <View style = {styles.view}>
+            <Text style ={styles.text}> Choose Your Chores </Text>
             {/* dropdown list button goes here */}
-            
-            <Button title="Open List" onPress ={() => { navigation.PushManager('ListScreen')}}/>
-            <Button title="Start New List" onPress = {() => {navigation.navigate('Home')}}/>
+            <View style ={styles.picker}>
+               <Picker 
+                    selectedValue={selectedValue}
+                    style={{ height: 50, width:250 }}
+                    onValueChange = {(itemValue, itemIndex) => 
+                    setSelectedValue(itemValue)}
+                    >
+                    <Picker.Item label = "Select Chore" />
+
+                    <Picker.Item label = "Hovering" value= "hovering" />
+                    <Picker.Item label = "Dishes" value= "dishes" />
+                    <Picker.Item label = "Laundry" value= "laundry" />
+                    <Picker.Item label = "Shopping" value= "shopping" />
+                    <Picker.Item label = "Walking Pet" value= "walkingPet" />
+
+
+
+               </Picker>
+
+
+            </View>
+            {/* <Button title="Open List" onPress ={() => { navigation.PushManager('ListScreen')}}/>
+            <Button title="Start New List" onPress = {() => {navigation.navigate('Home')}}/> */}
         </View>
+
+       
 
     )
 }
+
+const styles = StyleSheet.create({
+    view:{
+        alignItems: "center"
+
+    },
+    text:{
+        
+        alignItems: "center",
+        fontSize: 25,
+        alignContent: "center",
+        color: "green",
+        paddingTop:50
+    },
+    picker:{
+        flex: 1,
+        paddingTop: 40,
+        alignItems: "center"
+    }
+})
+
+
+
 
 export default SelectionScreen;
